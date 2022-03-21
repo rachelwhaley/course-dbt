faa801aebed64d28beb68027b7168eaa66b06877
rachelwhaley <rachelwhaley24@gmail.com>
2022-03-21 03:47:14 +0000

    adding tests for week 2

--- greenery/models/marts/core/dim_users.sql
+++ greenery/models/marts/core/dim_users.sql
@@ -16,11 +16,20 @@ addresses as (
     from {{ ref('stg_addresses') }}
 ),
 
+pageviews as (
+    select user_id,
+    count(event_id) as pageview_count
+    from {{ ref('stg_events')}}
+    where page_url is not null
+    group by 1
+),
+
 final as (
     select *
     from users
     left join orders using (user_id)
     left join addresses using (address_id)
+    left join pageviews using (user_id)
 )
 
 select * from final
